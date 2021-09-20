2b6f6225ec7041091210a9e1c37137c68891ab22
g.ibrahim.r <gryffindor.ibrahim@icloud.com>
2021-09-20 18:07:10 +0300

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by İbrahim Güler on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
